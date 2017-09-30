57bbf6d5f28a70f25809278988aae1bc8c0a7e59
kpphillips707 <kaimanaphillips23@gmail.com>
2017-09-29 17:52:42 -0700

    <push>

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    alert('Hello');
+}
+
+/*var ques1 = ($("input[@name=q1]:checked").val() != "c");
+
+var ques2 = ($("input[@name=q2]:checked").val() != "a");
+
+var ques3 = ($("input[@name=q3]:checked").val() != "d");
+
+var ques4 = ($("input[@name=q4]:checked").val() != "b");
+
+var ques5 = ($("input[@name=q5]:checked").val() != "c");
+
+*/
